80b1a7157d88f9b4196427c2f9d2bb7e3ed931a3
wjb-59 <wjb59@cam.ac.uk>
2026-02-02 17:51:37 +0000

    Q8 Attempted

--- Q8.py
+++ Q8.py
@@ -19,6 +19,14 @@ def part_a(x, y):
 
     return value
 
+
+# This code doesn't work with the monte carlo method - so doesn't work here
+def part_b(x, y=1):
+
+    value = np.sqrt(1-(x**2))*y
+
+    return value
+
 # Part a)
 print("Monte Carlo with N = 10^2:", monte_carlo(part_a, 10**2, 1, -1, 1, -1))
 print()
@@ -28,3 +36,9 @@ print("Monte Carlo with N = 10^6:", monte_carlo(part_a, 10**6, 1, -1, 1, -1))
 print()
 
 # Part b)
+#print("Monte Carlo with N = 10^2:", monte_carlo(part_b, 10**2, 1, -1, 1, -1))
+#print()
+#print("Monte Carlo with N = 10^5:", monte_carlo(part_b, 10**5, 1, -1, 1, -1))
+#print()
+#print("Monte Carlo with N = 10^6:", monte_carlo(part_b, 10**6, 1, -1, 1, -1))
+#print()
